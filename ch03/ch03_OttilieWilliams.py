80dd8f3ed539352f8baa805de51e8d06c169dbc0
Ottilie Williams <ottiliewilliams1@sky.com>
2018-12-03 13:51:40 +0000

    done up to task 5 in ch3 of curriculum

--- ch03/ch03_OttilieWilliams.py
+++ ch03/ch03_OttilieWilliams.py
@@ -17,10 +17,10 @@
 #def hello_world():
 #    print("Hello World!")
     
-def introduce():
-    name = input("What's your name? ")
-    print("My name is {}".format(name))
-    in_ten_years()
+#def introduce():
+#    name = input("What's your name? ")
+#    print("My name is {}".format(name.upper()))
+#    in_ten_years()
 
 #def where_are_you_from():
 #    city = input("What city are you from? ")
@@ -32,13 +32,68 @@ def introduce():
 #    print ("Your favourite food is {}".format(food))
 #    in_ten_years()
     
-def in_ten_years():
-    age = input("What's your age? ")
-    print("Your age is {} ".format(age))
-    add10 = int(age) + 10
-    print ("In ten years you will be "+ str(add10))
+#def in_ten_years():
+#    age = input("What's your age? ")
+#    print("Your age is {} ".format(age))
+#    add10 = int(age) + 10
+#    print ("In ten years you will be "+ str(add10))
+#    add20 = int(age) + 20
+#    print ("In twenty years you will be " + str(add20))
     
-    
+#def hello_world_2args(x, r, s):
+#        print ("{} {} {}".format(x, r, s))
+#
+#a1 = "hello"
+#b1 = "world"
+#c1 = "yo"
+#a2 = "love"
+#b2 = "coding"
+#c2 = "I do"
+#
+#hello_world_2args(a1, b1, c1)
+#hello_world_2args(a2, b2, c2)
 
-        
+#print (list(range(10)))
+#print (range(1,10))
+#print (range(1,10,2))
+
+#num1 = 15
+#num2 = 20
+
+#def add_two_numbers(num1, num2): 
+#    answer = num1 + num2
+#    print ("{} plus {} equals {}".format(num1, num2, answer))
+#    
+
+#def convert_temperature(centigrade):
+#    fahrenheit = centigrade * 9.0 / 5.0 + 32
+#    kelvin = centigrade + 273.15
+#    print ("Converting temperature in centigrade to fahrenheit and kelvin: ")  
+#    print ("Temperature in centigrade: ", centigrade)
+#    print ("Temperature in fahrenheit: ", fahrenheit)
+#    print ("Temperature in kelvin: ", kelvin)
+#    return kelvin + fahrenheit
+#
+#result = convert_temperature(50)
+#print (result + 5)
+
+def convert_distance(miles):
+    kilometers = (miles*8.0)/5.0
+    print ("Converting distance in miles to kilometers: ")
+    print ("Distance in miles: ", miles)
+    return kilometers
+
+London_ipswich = convert_distance(10)
+print ("Distance from London to Ipswich: ", London_ipswich)
+    
+#def convert_distance():
+#    miles = input ("Enter the miles you would like to convert: ")
+#    kilometers = (int(miles)*8.0)/5.0
+#    print ("Converting distance in miles to kilometers: ")
+#    print ("Distance in miles: ", miles)
+#    print ("Distance in kilometers: ", kilometers)
+#
+#convert_distance()
+
+
     
